537db99ee33f675c888e5eb88d66d1b4117aa8ed
Subhajit Roy <40741607+Subhajit-Roy-Partho@users.noreply.github.com>
2023-04-27 15:49:41 -0700

    added map

---
+++ map.cpp
@@ -0,0 +1,27 @@
+// Forms one to one map from one object to another as given below strings are maped to number.
+
+#include <iostream>
+#include <map>
+ 
+int main()
+{
+  // Create a map of strings to integers
+  std::map<std::string, int> map;
+ 
+  // Insert some values into the map
+  map["one"] = 1;
+  map["two"] = 2;
+  map["three"] = 3;
+ 
+  // Get an iterator pointing to the first element in the map
+  std::map<std::string, int>::iterator it = map.begin();
+ 
+  // Iterate through the map and print the elements
+  while (it != map.end())
+  {
+    std::cout << "Key: " << it->first << ", Value: " << it->second << std::endl;
+    ++it;
+  }
+ 
+  return 0;
+}
